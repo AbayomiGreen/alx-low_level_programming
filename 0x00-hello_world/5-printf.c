38ceb71cc21697f43103ee291e3a4170f803cd5a
Green Abayomi <greenadedeji@gmail.com>
2023-08-10 10:37:49 -0700

    Using the printf funtion to print a text

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - A program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
+ * main - A program that prints using printf function.
  *
  * Return: 0 (Success)
  */
