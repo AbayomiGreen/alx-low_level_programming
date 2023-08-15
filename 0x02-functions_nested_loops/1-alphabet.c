338ec2769f823ac72592c36da1dba1f5f4ee62d9
Green Abayomi <greenadedeji@gmail.com>
2023-08-15 04:24:41 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char e;
+
+	for (e = 'a'; e <= 'z'; e++)
+	{
+		_putchar(e);
+	}
+	_putchar('\n');
+}
+
