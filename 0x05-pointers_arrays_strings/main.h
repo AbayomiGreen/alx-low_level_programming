a24a85dc4d45f0ca807241ba6bdb41f609c6a0d8
Green A.A <greenadedeji@gmail.com>
2023-08-21 13:59:44 +0000

    task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _puts(char *str);
 int _strlen(char *s);
 void reset_to_98(int *n);
 int _putchar(char c);
 void swap_int(int *a, int *b);
+void _puts(char *str);
 
 #endif 
