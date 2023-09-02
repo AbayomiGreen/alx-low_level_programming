956281672ea8f2620ef3a3540f1262f3e47c02d6
Green A.A <greenadedeji@gmail.com>
2023-09-02 09:36:43 +0000

    static libraries

---
+++ 0x09-static_libraries/main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - calling _puts function
+ * Return: Always 0
+ */
+int main(void)
+{
+	_puts("\"At the end of the day,my goal was to be the best hacker\"\n\t-
+Kevin Mitnick");
+	return (0);
+}
